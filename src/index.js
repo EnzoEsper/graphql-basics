22ecee9dbf291213af2c27d8309427ead99baa29
Enzo Esper <enzoesper@hotmail.com>
2019-11-29 18:05:15 -0300

    subscription basics first test for practice

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import { GraphQLServer } from "graphql-yoga";
+import { GraphQLServer, PubSub } from "graphql-yoga";
 import db from "./db";
 import Query from "./resolvers/Query";
 import Mutation from "./resolvers/Mutation";
+import Subscription from "./resolvers/Subscription";
 import User from "./resolvers/User";
 import Post from "./resolvers/Post";
 import Comment from "./resolvers/Comment";
+
+const pubsub = new PubSub();
 
 // Resolvers
 const resolvers = {
   Query,
   Mutation,
+  Subscription,
   User,
   Post,
   Comment
@@ -19,7 +23,8 @@ const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
   resolvers,
   context: {
-    db
+    db,
+    pubsub
   }
 });
 
